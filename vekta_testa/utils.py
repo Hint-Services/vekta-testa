44eeceaeb45dde3459feacf9debbcccdab23a02a
Ben Hofferber <hofferber.ben@gmail.com>
2023-08-20 18:54:55 -0400

    :bookmark_tabs: add tabular results util

--- vekta_testa/utils.py
+++ vekta_testa/utils.py
@@ -1,4 +1,4 @@
-from typing import List, Tuple, Any, Dict
+from typing import Callable, List, Optional, Tuple, Any, Dict
 
 from vekta_testa.types import TestResult
 
@@ -29,7 +29,32 @@ def flatten_json(y):
 
 
 def default_find_id(entries: List[Tuple[Dict[str, Any], float]], target_id: str):
-    for index, (entry, score) in enumerate(entries):
+    for index, (entry, ) in enumerate(entries):
         if entry['id'] == target_id:
             return index
     return -1
+
+
+def tabularize_results(results: Dict[str, List[TestResult]], test_case: str, index_of_result: int, custom_scraper: Optional[Callable[[Any, float], List[Any]]] = None):
+    '''
+    Takes the results of a test case and returns a pandas dataframe with the top 10 results.
+
+    :param results: The results of the test cases.
+    :param test_case: The test case to tabularize.
+    :param index_of_result: The index of the result to tabularize.
+    :param custom_scraper: A function to scrape the results. Must return a list of values.
+
+    :returns: A pandas dataframe with the top 10 results.
+    '''
+
+    import pandas as pd
+    data = []
+    for (value, score) in (results[test_case][index_of_result]['all_results'][:10]):
+        if custom_scraper != None:
+            data.append(custom_scraper(value, score))
+        else:
+            data.append([score, value])
+
+    pd.set_option("display.max_colwidth", 10000)
+    df = pd.DataFrame(data)
+    return df
