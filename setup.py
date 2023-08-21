152e993b8842e31f622af87315f7995d9cb9b762
Ben Hofferber <hofferber.ben@gmail.com>
2023-08-20 20:07:11 -0400

    drop version from setup

--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@ from setuptools import setup, find_packages
 
 setup(
     name="vecta-testa",
-    version="0.1",
     packages=find_packages(),
     install_requires=[
         # Add your package dependencies here
